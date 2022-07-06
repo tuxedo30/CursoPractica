1f78315d81af85123db00c4e895543a9ded02efd
tuxedo <malvarezmontoyamanuela@cesde.net>
2022-07-05 22:41:36 -0500

    CursoPractica

--- CRUD.py
+++ CRUD.py
@@ -1,6 +1,7 @@
 from interfazgrafica import Interfaz
 
-datos = {}
 i = Interfaz()
-i.Pintar_Interfaz()
-
+i.mainloop()
+crear_actualiza = i.Crea_Actualiza('ELIMINAR')
+lee_elimina = i.Lee_Elimina
+print(crear_actualiza)
--- interfazgrafica.py
+++ interfazgrafica.py
@@ -1,72 +1,73 @@
 import tkinter as tk
+from tkinter import messagebox as mb
 
-from setuptools import Command
-
-from coneccionxqllite import Crear_BaseDatos
-
-
-class Interfaz:
-    
-    def Pintar_Interfaz(self):
-        #Interfaz gráfica
-        Root = tk.Tk()
-        Root.title('Datos de Usuario')
+class Interfaz(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title('Datos de Usuario')
         #Títulos
-        Titulo_ID = tk.Label(text='ID')
-        Titulo_ID.grid(row=0, column=0, padx=10)
-        Titulo_Nombre = tk.Label(text='Nombre')
-        Titulo_Nombre.grid(row=1, column=0, padx=10)
-        Titulo_Direccion = tk.Label(text='Dirección')
-        Titulo_Direccion.grid(row=1, column=0, padx=10)
-        Titulo_Password = tk.Label(text='Contraseña')
-        Titulo_Password.grid(row=2, column=0, padx=10)
-        Titulo_Comentario = tk.Label(text='Comentario')
-        Titulo_Comentario.grid(row=3, column=0, padx=10)
+        self.Titulo_ID = tk.Label(text='ID')
+        self.Titulo_ID.grid(row=0, column=0, padx=10)
+        self.Titulo_Nombre = tk.Label(text='Nombre')
+        self.Titulo_Nombre.grid(row=1, column=0, padx=10)
+        self.Titulo_Direccion = tk.Label(text='Dirección')
+        self.Titulo_Direccion.grid(row=2, column=0, padx=10)
+        self.Titulo_Password = tk.Label(text='Contraseña')
+        self.Titulo_Password.grid(row=3, column=0, padx=10)
+        self.Titulo_Comentario = tk.Label(text='Comentario')
+        self.Titulo_Comentario.grid(row=4, column=0, padx=10)
         
         #Entradas
-        Entrada_ID = tk.Entry()
-        Entrada_ID.grid(row=0, column=1, padx=(10, 10), pady=(10,10), columnspan=3)
-        Entrada_Nombre = tk.Entry()
-        Entrada_Nombre.grid(row=1, column=1, padx=(10, 10),pady=(10,10), columnspan=3)
-        Entrada_Direccion = tk.Entry()
-        Entrada_Direccion.grid(row=1, column=1, padx=(10, 10),pady=(10,10), columnspan=3)
-        Entrada_Password = tk.Entry()
-        Entrada_Password.grid(row=2, column=1, padx=(10, 10), pady=(10,10), columnspan=3)
-        Entrada_Comentario = tk.Text(height=10,width=20)
-        Entrada_Comentario.grid(ipadx=2,row=3, column=1, padx=(10, 10), pady=(10,10), columnspan=3)
+        self.Entrada_ID = tk.Entry()
+        self.Entrada_ID.grid(row=0, column=1, padx=(10, 10), pady=(10,10), columnspan=3)
+        self.Entrada_Nombre = tk.Entry()
+        self.Entrada_Nombre.grid(row=1, column=1, padx=(10, 10),pady=(10,10), columnspan=3)
+        self.Entrada_Direccion = tk.Entry()
+        self.Entrada_Direccion.grid(row=2, column=1, padx=(10, 10),pady=(10,10), columnspan=3)
+        self.Entrada_Password = tk.Entry()
+        self.Entrada_Password.grid(row=3, column=1, padx=(10, 10), pady=(10,10), columnspan=3)
+        self.Entrada_Comentario = tk.Text(height=10,width=20)
+        self.Entrada_Comentario.grid(ipadx=2,row=4, column=1, padx=(10, 10), pady=(10,10), columnspan=3)
         
         #Botonoes
-        Crear_Boton = tk.Button(text='Crear', command=self.Crear)
-        Crear_Boton.grid(row=4, padx=10, column=0, pady=10)
-        Leer_Boton = tk.Button(text='Leer', command=self.Leer)
-        Leer_Boton.grid(row=4, padx=10,column=1, pady=10)
-        Actualizar_Boton = tk.Button(text='Actualizar', command=self.Actualizar)
-        Actualizar_Boton.grid(row=4, padx=10, column=2, pady=10)
-        Eliminar_Boton = tk.Button(text='Eliminar', command=self.Eliminar)
-        Eliminar_Boton.grid(row=4, padx=10, column=3, pady=10)
+        self.Crear_Boton = tk.Button(text='Crear', command=lambda : self.Crea_Actualiza('CREAR'))
+        self.Crear_Boton.grid(row=5, padx=10, column=0, pady=10)
+        self.Leer_Boton = tk.Button(text='Leer', command=lambda: self.Lee_Elimina('LEER'))
+        self.Leer_Boton.grid(row=5, padx=10,column=1, pady=10)
+        self.Actualizar_Boton = tk.Button(text='Actualizar', command=lambda : self.Crea_Actualiza('ACTUALIZAR'))
+        self.Actualizar_Boton.grid(row=5, padx=10, column=2, pady=10)
+        self.Eliminar_Boton = tk.Button(text='Eliminar', command=lambda: self.Lee_Elimina('ELIMINAR'))
+        self.Eliminar_Boton.grid(row=5, padx=10, column=3, pady=10)
         
-        Root.mainloop()
+        #Popup
+        self.Popup = ''
+         
+    def Recolectar_info(self):
+        ID = self.Entrada_ID.get()
+        N = self.Entrada_Nombre.get()
+        P = self.Entrada_Password.get()
+        C = self.Entrada_Comentario.get("1.0","end-1c")
+        D = self.Entrada_Direccion.get()
+        if ID!='' and N!='' and P!='' and C!='' and D!='':
+            return [ID,N,P,C,D]
+        else:
+            self.Popup = mb.showinfo("Message","Debe llenar todos los campos")
         
-    def Crear(self):
-        pass
+            
+    def Recolectar_id(self):
+        ID = self.Entrada_ID.get()
+        if ID != '':
+            return ID
+        else:
+            self.Popup = mb.showinfo("Message","Debe especificar el ID")
+            e3ed      
+    def Crea_Actualiza(self, accion):
+        datos = self.Recolectar_info()
+        return [datos, accion]
+            
+    def Lee_Elimina(self, accion):
+        id = self.Recolectar_id()
+        return [id, accion]
+
     
-    def Leer(self):
-        pass
     
-    def Actualizar(self):
-        pass
-    
-    def Eliminar(self):
-        print('probando botones')
-    
-    def Recolectar_info(self,GUI):
-        datos = {}
-        datos.add.ID.get()
-        datos.add.N.get()
-        datos.add.D.get()
-        datos.add.P.get()
-        datos.add.C.get()
-        return datos
-    
-    def imprimir_info(self,datos, GUI):
-        pass
